e6f6577597efca1e10a1f5e4a110cfc8667d1e56
Marianna Mazzette <20104120@mail.wit.ie>
2023-12-21 13:43:45 +0000

    first commit

---
+++ IoT-RoomTempConSys/IoT-RoomTempConSys.ino
@@ -0,0 +1,21 @@
+#define BLYNK_TEMPLATE_ID "TMPL4Ttvyxrs2"
+#define BLYNK_TEMPLATE_NAME "Demo"
+#define BLYNK_AUTH_TOKEN "zZObzs-zRoociGIxehFtTP44L7nr10qM"
+
+#include <SPI.h>
+#include <BlynkSimpleWifi.h>
+#include <Arduino_MKRIoTCarrier.h>
+#include "secret.h"
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASSWORD;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
